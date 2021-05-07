e2c2d3c005cc84d873195cc45dcf1605e8d34346
Sofya Kiseleva-Komolkina <skomolkina@gmail.com>
2021-05-07 21:37:33 +0300

    added page calendar without js

---
+++ scripts/calendar.js
@@ -0,0 +1,24 @@
+// const popup = document.querySelector('.popup');
+
+// const popupMeet = popup.querySelector('.popup__meet');
+// const buttonOpenCalendarPopup = popupMeet.querySelector('.button_place_calendar-open');
+// // const buttonCloseCalendarPopup = calendarPopup.querySelector('.calendar__popup_button_close');
+
+
+// const closeWithClick = (evt) => {
+//     // const calendarPopup = document.querySelector('.popup-calendar');
+//     if (evt.target.classList.contains('popup__meet') || (evt.target.classList.contains('calendar__button_close'))) {
+//         closePopup(popup)
+//     }
+// }
+// const openPopup = (popup) => {
+//     popup.classList.add('popup_opened');
+// }
+// const closePopup = (popup) => {
+//     popup.classList.remove('popup_opened');
+// }
+
+
+// buttonOpenCalendarPopup.addEventListener('click', () => {
+//     openPopup(popup);
+// });
